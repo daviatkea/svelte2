01f601fd322e9bb2e9dc55f326e3faaa1860daf9
Dannie Vinther <davi@kea.dk>
2022-03-11 09:59:56 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,97 @@
+<script>
+  import Button from "./Button.svelte";
+</script>
+
+<header>
+  <h1>Mine components</h1>
+</header>
+
+<main>
+  <section>
+    <h2>Buttons</h2>
+
+    <article>
+      <div>
+        <h3>Primary</h3>
+        <Button>Primary knap</Button>
+        <pre><code
+            >&lt;script&gt;
+  import Button from "./Button.svelte";
+&lt;/script&gt;
+
+&lt;Button type="primary"&gt;Første knap&lt;/Button&gt;</code
+          ></pre>
+      </div>
+      <hr />
+      <div>
+        <h3>Secondary</h3>
+
+        <Button type="secondary">Første knap</Button>
+        <pre><code>{`<Button type="secondary">Første knap</Button>`}</code
+          ></pre>
+      </div>
+      <hr />
+      <div>
+        <h3>Primary inversed</h3>
+
+        <Button type="primary" inversed={true}>Første knap</Button>
+        <pre><code
+            >{`<Button type="primary" inversed={true}>Første knap</Button>`}</code
+          ></pre>
+      </div>
+      <hr />
+      <div>
+        <h3>Secondary inversed</h3>
+
+        <Button type="secondary" inversed={true}>Første knap</Button>
+        <pre><code
+            >{`<Button type="secondary" inversed={true}>Første knap</Button>`}</code
+          ></pre>
+      </div>
+    </article>
+  </section>
+
+  <section>
+    <h2>Buttons</h2>
+
+    <article>
+      <div>
+        <h3>Primary</h3>
+        <Button>Primary knap</Button>
+        <pre><code
+            >&lt;script&gt;
+  import Button from "./Button.svelte";
+&lt;/script&gt;
+
+&lt;Button type="primary"&gt;Første knap&lt;/Button&gt;</code
+          ></pre>
+      </div>
+      <hr />
+      <div>
+        <h3>Secondary</h3>
+
+        <Button type="secondary">Første knap</Button>
+        <pre><code>{`<Button type="secondary">Første knap</Button>`}</code
+          ></pre>
+      </div>
+      <hr />
+      <div>
+        <h3>Primary inversed</h3>
+
+        <Button type="primary" inversed={true}>Første knap</Button>
+        <pre><code
+            >{`<Button type="primary" inversed={true}>Første knap</Button>`}</code
+          ></pre>
+      </div>
+      <hr />
+      <div>
+        <h3>Secondary inversed</h3>
+
+        <Button type="secondary" inversed={true}>Første knap</Button>
+        <pre><code
+            >{`<Button type="secondary" inversed={true}>Første knap</Button>`}</code
+          ></pre>
+      </div>
+    </article>
+  </section>
+</main>
---
+++ src/Button.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let type = "default";
+  export let inversed = false;
+</script>
+
+<button class:inversed class={type}><slot /></button>
+
+<style>
+  button {
+    border: none;
+    color: var(--primary-font-color);
+    background: var(--default-color);
+    padding: 1rem;
+    border-radius: 12px;
+    transition: all 0.2s;
+    cursor: pointer;
+    max-width: -moz-fit-content;
+    max-width: fit-content;
+  }
+  .primary {
+    background: var(--primary-color);
+  }
+  .primary:hover {
+    color: var(--primary-color);
+    background: var(--primary-font-color);
+  }
+  .secondary {
+    background: var(--secondary-color);
+  }
+  .primary.inversed {
+    color: var(--primary-color);
+    background: var(--primary-font-color);
+  }
+  .secondary.inversed {
+    color: var(--secondary-color);
+    background: var(--primary-font-color);
+  }
+</style>
